0c98b6c45781ebd4db642f087eee76daefcf25e8
Marco Chiang <mchiang@student.umass.edu>
2013-03-13 14:22:26 -0400

    added Tweet button with hover over animations as well as blurring out text on focus of the text area

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-$('#glance #newTweet').focus(function () {
+/*$('#glance #newTweet').focus(function () {
     $(this).animate({ height: "100px" }, 500);
 	if($(this).text() === 'Compose a new tweet...') {
 		$(this).text('');
@@ -13,4 +13,22 @@ $('#glance #newTweet').focusout(function () {
 		$(this).animate({ height: "16px" }, 500);
 		$(this).text('Compose a new tweet...');
 	}
+});*/
+
+$('input.tweetButton').hide();
+
+
+$('textarea.tweetExpand').focus(function () {
+	$(this).animate({ height: "100px" }, 0);
+	$('input.tweetButton').show();
+	$('input.tweetButton').hover(function () {
+		$(this).attr("src","/img/Tweet_Focus_In.jpg");
+	}, function() {
+		$(this).attr("src","/img/Tweet_Focus_Out.jpg");
+	});
+});
+
+$('textarea.tweetExpand').focusout(function () {
+	$(this).animate({ height: "16px" }, 0);
+	$('input.tweetButton').hide();
 });
